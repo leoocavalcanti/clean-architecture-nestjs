e6df787bac0190fc830ee768222decdfa91e1fd7
Leonardo Cavalcanti <leonardohenrique521@gmail.com>
2023-12-10 15:45:00 -0300

    test: adicionando entity fake builder para mock de testes

---
+++ src/users/domain/entities/testing/helpers/user-data-builder.ts
@@ -0,0 +1,20 @@
+import { UserProps } from './../../user.entity';
+import { faker } from '@faker-js/faker';
+
+type Props = {
+  name?: string;
+  email?: string;
+  password?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export function UserDataBuilder(props: Props): UserProps {
+  return {
+    name: props.name ?? faker.person.firstName(),
+    email: props.email ?? faker.internet.email(),
+    password: props.password ?? faker.internet.password(),
+    createdAt: props.createdAt ?? new Date(),
+    updatedAt: props.updatedAt ?? new Date(),
+  };
+}
